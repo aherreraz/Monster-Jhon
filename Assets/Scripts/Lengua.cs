383fb6fedfcf26c69bc983d00b0a8ef1bdad517d
Andres Herrera <arhz29@gmail.com>
2018-01-27 04:59:15 -0500

    nivel2 -scripts para lengua y mosca

--- Assets/Scripts/Lengua.cs
+++ Assets/Scripts/Lengua.cs
@@ -1,52 +1,41 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lengua : MonoBehaviour {
-
-	private Vector3 screenMousePosition;
+public class Lengua : MonoBehaviour
+{
+    private Vector3 screenMousePosition;
 	private Vector2 launchVector;
 	public GameObject lengua;
+    
+    public IEnumerator MostrarLengua(float t)
+    {
+        DibujarLengua();
+        lengua.GetComponent<SpriteRenderer>().enabled = true;
+        // TODO calcular collision
+        yield return new WaitForSeconds(t);
+        lengua.GetComponent<SpriteRenderer>().enabled = false;
+    }
 
-	public void ResetValues()
-	{
-		GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-		GetComponent<LineRenderer>().SetPosition(0, Vector3.zero);
-		GetComponent<LineRenderer>().SetPosition(1, Vector3.zero);
-	}
-
-	public void Launch (Vector2 launchDirection)
-	{
-		GetComponent<Rigidbody2D>().velocity = launchDirection;
-	}
-
-
-	public void DrawLaunchVector()
+	public void DibujarLengua()
 	{
 		screenMousePosition = new Vector2(Input.mousePosition.x / Screen.width * 16, Input.mousePosition.y / Screen.height * 9);
 		Vector3[] positions = { transform.position, screenMousePosition };
 		launchVector = screenMousePosition - transform.position;
-		//print(Mathf.Atan2(launchVector.y, launchVector.x));
+        print(launchVector);
 		float atan = Mathf.Atan2(launchVector.y, launchVector.x);
 		float angle = atan / Mathf.PI * 180.0f;
-		print(angle);
-		lengua.transform.rotation = Quaternion.Euler(new Vector3( 0, 0, (angle + 270)));
-		//GetComponent<LineRenderer>().SetPositions(positions);
+		lengua.transform.rotation = Quaternion.Euler(new Vector3( 0, 0, angle + 270));
 		lengua.transform.position = transform.position;
 		lengua.transform.localScale = new Vector3(1.0f, 1.0f * launchVector.magnitude);
-		//
-		//
 	}
-	public void ToggleLaunchVector()
-	{
-		GetComponent<LineRenderer>().enabled = !GetComponent<LineRenderer>().enabled;
-	}
-	public Vector2 GetLaunchDirection()
-	{
-		return launchVector.normalized;
-	}
-	public float GetLaunchStrength()
-	{
-		return launchVector.magnitude;
-	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("click");
+            StartCoroutine(MostrarLengua(.5f));
+        }
+    }
 }
---
+++ Assets/Scripts/Mosca.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mosca : MonoBehaviour
+{
+    public float velocidad;
+    public float tMin, tMax;
+    private Vector3 movimiento;
+	
+	private void Start ()
+    {
+        StartCoroutine(CambiarDireccion());
+	}
+	
+	void Update ()
+    {
+        transform.Translate(movimiento * velocidad * Time.deltaTime);
+	}
+    private IEnumerator CambiarDireccion()
+    {
+        while (true)
+        {
+            movimiento.x = Random.Range(-1.0f, 1.0f);
+            movimiento.y = Random.Range(-1.0f, 1.0f);
+            yield return new WaitForSeconds(Random.Range(tMin, tMax));
+        }
+    }
+}
