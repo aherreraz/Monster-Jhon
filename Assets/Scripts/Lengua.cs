940bce4e5a800d350a0afeaa952b740a2c876ff9
Andres Herrera <arhz29@gmail.com>
2018-01-27 06:34:53 -0500

    Comer moscas y spawner

--- Assets/Scripts/Lengua.cs
+++ Assets/Scripts/Lengua.cs
@@ -7,34 +7,37 @@ public class Lengua : MonoBehaviour
     private Vector3 screenMousePosition;
 	private Vector2 launchVector;
 	public GameObject lengua;
+    public GameObject finLengua;
     
     public IEnumerator MostrarLengua(float t)
     {
         DibujarLengua();
         lengua.GetComponent<SpriteRenderer>().enabled = true;
+        finLengua.GetComponent<SpriteRenderer>().enabled = true;
         // TODO calcular collision
         yield return new WaitForSeconds(t);
         lengua.GetComponent<SpriteRenderer>().enabled = false;
+        finLengua.GetComponent<SpriteRenderer>().enabled = false;
+        finLengua.transform.position = new Vector2(-1, -1);
     }
 
 	public void DibujarLengua()
 	{
 		screenMousePosition = new Vector2(Input.mousePosition.x / Screen.width * 16, Input.mousePosition.y / Screen.height * 9);
-		Vector3[] positions = { transform.position, screenMousePosition };
 		launchVector = screenMousePosition - transform.position;
-        print(launchVector);
+        //print(launchVector);
 		float atan = Mathf.Atan2(launchVector.y, launchVector.x);
 		float angle = atan / Mathf.PI * 180.0f;
 		lengua.transform.rotation = Quaternion.Euler(new Vector3( 0, 0, angle + 270));
 		lengua.transform.position = transform.position;
 		lengua.transform.localScale = new Vector3(1.0f, 1.0f * launchVector.magnitude);
+        finLengua.transform.position = screenMousePosition;
 	}
 
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
-            print("click");
             StartCoroutine(MostrarLengua(.5f));
         }
     }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Rect areaSpawn;
+    public float tMin, tMax;
+    public Mosca mosca;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnMoscas());
+    }
+
+    public IEnumerator SpawnMoscas()
+    {
+        while (true)
+        {
+            Vector3 posicion = new Vector3(
+                Random.Range(transform.position.x - areaSpawn.width / 2, transform.position.x + areaSpawn.width / 2),
+                Random.Range(transform.position.y - areaSpawn.height / 2, transform.position.y + areaSpawn.height / 2),
+                0);
+            Instantiate(mosca, posicion, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(tMin, tMax));
+        }
+    }
+}
