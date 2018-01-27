17e08a96e1855bed61fa318cbb28764b4df83965
Andres Herrera <arhz29@gmail.com>
2018-01-27 13:46:03 -0500

    Menu principal

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+	private void Awake ()
+    {
+		if (!instance)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+	}
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public IEnumerator ReloadLevel(float t)
+    {
+        yield return new WaitForSeconds(t);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            LevelManager.instance.LoadNextLevel();
+    }
+}
